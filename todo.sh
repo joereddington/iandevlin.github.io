7bcd55ee3a473539d7efbd4c83822fb8bae3d753
Joe Reddington <joe@joereddington.com>
2015-08-28 11:47:56 +0100

    adding todo script

---
+++ todo.sh
@@ -0,0 +1,12 @@
+#So what do we have to do? 
+
+#First thing is that we download the files that are there. 
+#
+#
+#Then we convert them into files that can be played by our player. 
+#
+#
+#Then we have a way of displaying them. 
+#---Firstly by just letting the user select all the subtitles for one video
+#---Then by having a set of links to videos
+#---For each downloaded file we also want a html file, insterting the correct filenames in.  It's going to be a php file eventually, but this will be quick enought
