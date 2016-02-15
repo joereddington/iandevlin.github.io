2ac1b93d11e440e1de6ac107c1b219e1eab7404f
Joe Reddington <joe@joereddington.com>
2016-02-15 19:09:55 +0000

    percentages are now being output in text

--- download_and_create_html.py
+++ download_and_create_html.py
@@ -4,13 +4,14 @@ import get_files
 import os
 import argparse
 
+
 def setup_argument_list():
-     "creates and parses the argument list for naCount"
-     parser = argparse.ArgumentParser(
-         description=__doc__)
-     parser.add_argument('-u', nargs="?",dest='url',
-                         help="Updates one particular URL")
-     return parser.parse_args()
+        "creates and parses the argument list for naCount"
+        parser = argparse.ArgumentParser(
+            description=__doc__)
+        parser.add_argument('-u', nargs="?", dest='url',
+                            help="Updates one particular URL")
+        return parser.parse_args()
 
 
 def get_gformat_subs(filename):
@@ -18,38 +19,41 @@ def get_gformat_subs(filename):
         intended to parse files form google drive"""
         with open(filename, 'rb') as subs_file:
                 reader = csv.reader(subs_file, skipinitialspace=True)
-                headers=next(reader, None)[3:]
-                print "Headers", headers
+                headers = next(reader, None)[3:]
+#                print "Headers", headers
                 lines = filter(None, reader)
-                return (headers,lines)
+                return (headers, lines)
 
 
 def convert_sup_to_srt(filename):
-        names=[]
-        column=2
+        names = []
+        column = 2
         try:
-                print "Examining: "+filename
+#                print "Examining: "+filename
                 (headers, group) = get_gformat_subs(filename)
-                print "Headers", headers
+                #print "Headers", headers
                 for language in headers:
-                    subs = pysrt.SubRipFile()
-                    print "We are currently looking at", language
-                    column=column+1
-                    tag=language.replace(" ","_")
-                    for line in group:
-                            current_sub = pysrt.SubRipItem()
-                            current_sub.start = line[0].replace(',', '.')
-                            current_sub.end = line[1].replace(',', '.')
-                            current_sub.text = line[column].decode('latin-1')
-                            subs.append(current_sub)
-
-                    subs.save('temp.vtt')
-                    new_filename = 'live/subtitles/' + \
-                        os.path.splitext(os.path.basename(filename))[0]+tag+'.vtt'
-                    print "Created:" + new_filename
-                    os.system('echo WEBVTT > '+new_filename)
-                    os.system('cat temp.vtt >> '+new_filename)
-                    names.append(new_filename)
+                        subs = pysrt.SubRipFile()
+                       # print "We are currently looking at", language
+                        column = column+1
+                        tag = language.replace(" ", "_")
+                        for line in group:
+                                if (len(line[column]) > 1):
+                                        current_sub = pysrt.SubRipItem()
+                                        current_sub.start = line[
+                                                0].replace(',', '.')
+                                        current_sub.end = line[
+                                                1].replace(',', '.')
+                                        current_sub.text = line[
+                                                column].decode('latin-1')
+                                        subs.append(current_sub)
+                        subs.save('temp.vtt')
+                        new_filename = 'live/subtitles/' + os.path.splitext(
+                                os.path.basename(filename))[0]+tag+'.vtt'
+#                        print "Created:" + new_filename
+                        os.system('echo WEBVTT > '+new_filename)
+                        os.system('cat temp.vtt >> '+new_filename)
+                        names.append((new_filename, len(subs), language))
                 return names
         except IndexError:
                 print "I'm Sorry - wrong sort of file :( "
@@ -57,7 +61,7 @@ def convert_sup_to_srt(filename):
 
 
 def create_webpage(filename):
-        print  "Create_webpage in:", filename
+#        print "Create_webpage in:", filename
         """Creates a html file inserts the subtitle filename and saves it with
         the correct filename"""
         mastertext = """
@@ -76,34 +80,40 @@ def create_webpage(filename):
       <script src="js/video-player.js"></script>
    </body>
 </html>""".format(filename)
-        new_filename = "live/"+os.path.splitext(os.path.basename(filename))[0]+'.html'
+        new_filename = "live/"+os.path.splitext(
+            os.path.basename(filename))[0]+'.html'
         print new_filename
         with open(new_filename, "w") as myfile:
                 myfile.write(mastertext)
 
 
-
 def process_subfile(filename):
-    "Subfiles might produce many many html files"
-    print filename
-    return
-    newfilenames = convert_sup_to_srt(filename)
-    for newfilename in newfilenames:
-        print "New filename is: ", newfilename
-        create_webpage(newfilename)
+        "Subfiles might produce many many html files"
+     #   print "here" + filename
+        newfilenames = convert_sup_to_srt(filename)
+        total_utterances = newfilenames[0][1]
+     #   print total_utterances
+        for newfilename in newfilenames:
+     #           print newfilename[0]
+#                print newfilename[1]
+                percentage_complete = float(
+                    newfilename[1])/float(total_utterances)*100
+                print "{} version is  {}% complete with filename: {}".format(newfilename[2], percentage_complete, newfilename[0])
+                create_webpage(newfilename[0])
+
 
 def convert_folder_into_html(files_downloaded):
-    " Then we convert them into files that can be played by our player."
-    for filename in files_downloaded:
-        process_subfile(filename)
+        """ Then we convert them into files that can be played by our player.
+        while also creating the table of thier information"""
+        for filename in files_downloaded:
+                process_subfile(filename)
 
 # First thing is that we download the files that are there.
 #
-args=setup_argument_list()
+args = setup_argument_list()
 if args.url:
-    url=args.url
-    filename=get_files.download_file(args.url)
-    process_subfile(filename)
+        url = args.url
+        filename = get_files.download_file(args.url)
+        process_subfile(filename)
 else:
-    convert_folder_into_html(get_files.download_folder())
-
+        convert_folder_into_html(get_files.download_folder())
